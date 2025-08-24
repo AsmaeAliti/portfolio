1b1422f717fdfe57f72d2487bd7849a7d31bdaee
AsmaeAliti <alitiasmae@gmail.com>
2025-08-24 22:47:41 +0000

    remove contact sectiona and add sky bacckground to the eyes section

--- index.js
+++ index.js
@@ -29,24 +29,6 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   });
 });
 
-// Form submission handler
-// const contactForm = document.getElementById('contact-form');
-// contactForm.addEventListener('submit', function(e) {
-//     e.preventDefault();
-
-//     // Get form values
-//     const name = document.getElementById('name').value;
-//     const email = document.getElementById('email').value;
-//     const subject = document.getElementById('subject').value;
-//     const message = document.getElementById('message').value;
-
-//     // In a real application, you would send this data to a server
-//     // For this demo, we'll just show an alert
-//     alert(`Thank you, ${name}! Your message has been received. I'll get back to you soon.`);
-
-//     // Reset the form
-//     contactForm.reset();
-// });
 
 (function () {
   var width,
